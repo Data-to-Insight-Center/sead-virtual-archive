fe7309ee85ad85d1e5473c59221e7ed2bd1dca6f
kavchand <rumpelstiltskin>
2014-03-04 13:50:05 -0500

    Creating SEAD Bag from not-holey bag. Added at /OreDataBag

--- SEAD-VA-extensions/services/bagItRestService/src/main/java/org/seadva/bagit/BagItUtilService.java
+++ SEAD-VA-extensions/services/bagItRestService/src/main/java/org/seadva/bagit/BagItUtilService.java
@@ -48,10 +48,20 @@ public class BagItUtilService {
     @Context
     ServletContext context;
 
+    /**
+     * Method to generate a bag with ORE when user supplies a holey BagIt Bag (without ORE)
+     * @param uploadedInputStream
+     * @param fileDetail
+     * @return
+     * @throws IOException
+     * @throws OREException
+     * @throws URISyntaxException
+     */
+
     @POST
     @Path("/OreBag")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response getORE(
+    public Response getHoleyOREBag(
             @FormDataParam("file") InputStream uploadedInputStream,
             @FormDataParam("file") FormDataContentDisposition fileDetail
     ) throws IOException, OREException, URISyntaxException
@@ -69,7 +79,8 @@ public class BagItUtilService {
             StringWriter writer = new StringWriter();
             IOUtils.copy(new FileInputStream(
                     context.getRealPath("/WEB-INF/Config.properties")
-            ), writer);
+             ), writer);
+
             String result = writer.toString();
             String[] pairs = result.trim().split(
                     "\\w*\n|\\=\\w*");
@@ -119,6 +130,90 @@ public class BagItUtilService {
         }
         return null;
     }
+
+    /**
+     * Method to generate a not-holey bag with ORE when user supplies a not-holey BagIt Bag (without ORE)
+     * @param uploadedInputStream
+     * @param fileDetail
+     * @return
+     * @throws IOException
+     * @throws OREException
+     * @throws URISyntaxException
+     */
+
+    @POST
+    @Path("/OreDataBag")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response getOREBag(
+            @FormDataParam("file") InputStream uploadedInputStream,
+            @FormDataParam("file") FormDataContentDisposition fileDetail
+    ) throws IOException, OREException, URISyntaxException
+
+    {
+        if(!fileDetail.getFileName().endsWith(".zip"))
+            return Response
+                    .status(Response.Status.NOT_ACCEPTABLE)
+                    .header("SEAD-Exception-Name", "Not accepted")
+                    .entity("Please upload a zipped SEAD BagIt file.")
+                    .type(MediaType.APPLICATION_XML)
+                    .build();
+
+        if(Constants.homeDir==null){
+            StringWriter writer = new StringWriter();
+            IOUtils.copy(new FileInputStream(
+                    context.getRealPath("/WEB-INF/Config.properties")
+                 ), writer);
+
+            String result = writer.toString();
+            String[] pairs = result.trim().split(
+                    "\\w*\n|\\=\\w*");
+
+
+            for (int i = 0; i + 1 < pairs.length;) {
+                String name = pairs[i++].trim();
+                String value = pairs[i++].trim();
+                if (name.equals("bagit.home")) {
+                    Constants.homeDir = value;
+                }
+            }
+        }
+
+        Constants.unzipDir = Constants.homeDir+""+"unzip";
+        String zippedBag = Constants.unzipDir+"/"+fileDetail.getFileName();
+        String unzipDir = Constants.unzipDir+"/"+
+                fileDetail.getFileName().replace(".zip","")+"/";
+
+        String sipPath;
+        try {
+            if(!(new File(unzipDir).exists()))
+                (new File(unzipDir)).mkdirs();
+
+            writeToFile(uploadedInputStream,zippedBag);
+
+            ZipUtil.unzip(zippedBag, unzipDir);
+
+
+            AcrBagItConverter acrBagItConverter = new AcrBagItConverter();
+            String finalZippedBag = acrBagItConverter.convertDirectoryToORE(new File(unzipDir+"/data"));
+
+            if(zippedBag==null)
+                throw new NotFoundException("Failed to create a zipped BagIt Bag. Please ensure the basic bag you uploaded is valid.");
+
+            String[] filename = finalZippedBag.split("/");
+
+            Response.ResponseBuilder responseBuilder = Response.ok(new FileInputStream(finalZippedBag));
+
+            responseBuilder.header("Content-Type", "application/zip");
+            responseBuilder.header("Content-Disposition",
+                    "inline; filename=" + filename[filename.length-1]);
+            return responseBuilder.build();
+        }
+        catch (JSONException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return null;
+    }
+
 
     private void writeToFile(InputStream uploadedInputStream,
                              String uploadedFileLocation) {
