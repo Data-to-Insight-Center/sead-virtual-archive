65d1b22d8ca3881411f93253ae2764ff113d6eaa
kavchand <kavchand@indiana.edu>
2014-04-17 15:50:07 -0400

    Deprecating older code without hibernate.

---
+++ SEAD-VA-extensions/sead-registry/sql/type_registry_insert.sql
@@ -0,0 +1,43 @@
+insert into metadata_type values('md:1',
+'http://purl.org/dc/terms/','title'
+);
+insert into metadata_type values('md:2',
+'http://purl.org/dc/terms/','creator'
+);
+insert into metadata_type values('md:3',
+'http://purl.org/dc/terms/','identifier'
+);
+insert into metadata_type values('md:4',
+'http://purl.org/dc/terms/','hasPart'
+);
+insert into metadata_type values('md:5',
+'http://purl.org/dc/elements/1.1/','format'
+);
+insert into metadata_type values('md:6',
+'http://purl.org/dc/terms/','SizeOrDuration'
+);
+insert into metadata_type values('md:7',
+'http://purl.org/dc/terms/','issued'
+);
+insert into metadata_type values('md:8',
+'http://purl.org/dc/terms/','mediator'
+);
+insert into metadata_type values('md:9',
+'http://purl.org/dc/terms/','Location'
+);
+insert into metadata_type values('md:10',
+'http://purl.org/dc/terms/','abstract'
+);
+insert into metadata_type values('md:11',
+'http://purl.org/dc/terms/','source'
+);
+insert into metadata_type values('md:12',
+'http://www.loc.gov/METS/','FLocat'
+);
+
+insert into base_entity values('state:1', 'PO',
+'2014-01-01 00:00:00', '2014-01-01 00:00:00');
+
+insert into state values('state:1',
+'PO','PublishedObject'
+);
