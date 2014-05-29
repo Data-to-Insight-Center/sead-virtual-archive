1e273868f02c8d9ddc56687dd2b23494f61fda3b
= <avaradha@indiana.edu>
2014-05-29 06:26:29 -0400

    Made changes to create a tar file to be deposited into SDA for DPN

--- SEAD-VA-extensions/dcs-ingest/sead-ingest-services/src/main/java/org/dataconservancy/dcs/ingest/services/TarUtilService.java
+++ SEAD-VA-extensions/dcs-ingest/sead-ingest-services/src/main/java/org/dataconservancy/dcs/ingest/services/TarUtilService.java
@@ -7,22 +7,33 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.compress.utils.IOUtils;
 import org.apache.commons.compress.archivers.ArchiveOutputStream;
 import org.apache.commons.compress.archivers.ArchiveStreamFactory;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.dataconservancy.dcs.ingest.Events;
+import org.dataconservancy.model.builder.InvalidXmlException;
 import org.dataconservancy.model.dcs.DcsDeliverableUnit;
 //import org.dataconservancy.model.dcs.DcsResourceIdentifier;
+import org.dataconservancy.model.dcs.DcsEntityReference;
+import org.dataconservancy.model.dcs.DcsEvent;
 import org.seadva.model.SeadDataLocation;
 import org.seadva.model.SeadDeliverableUnit;
+import org.seadva.model.builder.xstream.SeadXstreamStaxModelBuilder;
 import org.seadva.model.pack.ResearchObject;
 
 public class TarUtilService extends IngestServiceBase implements IngestService {
+    String dirPath;
+    String tarFileName = "dpntest.tar";
+    String tarFilePath;
     public void execute(String sipRef) {
 
+        System.out.println("SIP Ref in TarUtilService: "+sipRef);
         ResearchObject sip = (ResearchObject)ingest.getSipStager().getSIP(sipRef);
+
 
         Collection<DcsDeliverableUnit> dus = sip.getDeliverableUnits();
 
@@ -30,9 +41,26 @@ public class TarUtilService extends IngestServiceBase implements IngestService {
 
             if(d.getParents() ==null ||d.getParents().isEmpty())  {
                 SeadDataLocation dataLocation = ((SeadDeliverableUnit) d).getPrimaryLocation();
-                dataLocation.getLocation();
+                dirPath = dataLocation.getLocation();
+                File tarDirectory = new File(dirPath);
+                tarFilePath = dirPath.toString()+tarFileName;
+                this.tarDirectory(tarDirectory,tarFilePath);
+
             }
         }
+        System.out.println("TarFilePath in TarUtilService: "+tarFilePath);
+
+        for(DcsDeliverableUnit du :sip.getDeliverableUnits())
+        {
+            SeadDataLocation location = new SeadDataLocation();
+            location.setLocation(tarFilePath); //for tar requirement
+            location.setName("filepath");
+            location.setType("filepath");
+            ((SeadDeliverableUnit) du).setPrimaryLocation(location);
+        }
+
+        ingest.getSipStager().updateSIP(sip,sipRef);
+        addTarEvent(sipRef);
     }
 
     public void unTarFile(String inputTarPath, String destinationDirectory)throws IOException {
@@ -68,12 +96,19 @@ public class TarUtilService extends IngestServiceBase implements IngestService {
         }
 
     }
+
+    /**
+     * This method is used to create a tar file from the contents of the
+     * directory being passed with the file name being passed.
+     * @param dir
+     * @param tarFileName
+     */
     List<String> filesListInDir;
-    public void tarDirectory(File dir, String tarDirName){
+    public void tarDirectory(File dir, String tarFileName){
         try{
             filesListInDir = new ArrayList<String>();
             getFilesList(dir);
-            FileOutputStream fos = new FileOutputStream(tarDirName);
+            FileOutputStream fos = new FileOutputStream(tarFileName);
             ArchiveOutputStream aos = new ArchiveStreamFactory().createArchiveOutputStream(ArchiveStreamFactory.TAR, fos);
             for(String filePath : filesListInDir){
                 File inputFile = new File(filePath);
@@ -103,4 +138,11 @@ public class TarUtilService extends IngestServiceBase implements IngestService {
             else getFilesList(file);
         }
     }
+
+    private void addTarEvent(String sipRef) {
+        DcsEvent tarEvent = ingest.getEventManager().newEvent(Events.TARBAG);
+        ResearchObject dcp = (ResearchObject)ingest.getSipStager().getSIP(sipRef);
+        ingest.getEventManager().addEvent(sipRef, tarEvent);
+    }
 }
+
