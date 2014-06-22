da675e865783789c654323f8181e3a2e8e032e94
avaradha <avaradha@indiana.edu>
2014-06-22 18:31:18 -0400

    Removed the code which updates the bag size in the bag-infotxt file. This is now being done in TarUtilService

--- SEAD-VA-extensions/dcs-ingest/sead-ingest-services/src/main/java/org/dataconservancy/dcs/ingest/services/MiscService.java
+++ SEAD-VA-extensions/dcs-ingest/sead-ingest-services/src/main/java/org/dataconservancy/dcs/ingest/services/MiscService.java
@@ -1,11 +1,13 @@
 package org.dataconservancy.dcs.ingest.services;
 
 import java.io.*;
+import java.security.MessageDigest;
 import java.util.*;
 
 import org.dataconservancy.model.dcs.DcsDeliverableUnit;
 import org.dataconservancy.model.dcs.DcsFile;
 import org.dataconservancy.model.dcs.DcsFixity;
+import org.dataconservancy.model.dcs.DcsResourceIdentifier;
 import org.seadva.model.SeadDataLocation;
 import org.seadva.model.SeadDeliverableUnit;
 import org.seadva.model.pack.ResearchObject;
@@ -16,7 +18,9 @@ public class MiscService extends IngestServiceBase implements IngestService {
     String manifestFilePath;
     Writer bagInfo;
     Writer manifestInfo;
+    Writer manifest;
     File bagDirectory;
+
     public void execute(String sipRef) {
         long bagSize;
         ResearchObject sip = (ResearchObject)ingest.getSipStager().getSIP(sipRef);
@@ -27,47 +31,87 @@ public class MiscService extends IngestServiceBase implements IngestService {
                 SeadDataLocation dataLocation = ((SeadDeliverableUnit) d).getPrimaryLocation();
                 dirPath = dataLocation.getLocation();
                 bagDirectory = new File(dirPath);
-                FileWriter bagInfoStream = null;
-                bagSize = ((SeadDeliverableUnit)d).getSizeBytes();
-                try {
-                    String bagInfoTxtFilePath = bagDirectory.toString() + "/bag-info.txt";
-                    bagInfoStream = new FileWriter(bagInfoTxtFilePath,true);
-                    bagInfo = new BufferedWriter(bagInfoStream);
-                    bagInfo.write("Bag-Size: "+bagSize+" Bytes");
-                    bagInfo.close();
 
-                } catch (IOException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                }
-                System.out.println("Misc Service - bagSize is "+bagSize);
-            }
-        }
+                Collection<DcsFile> dcsFiles = sip.getFiles();
+                manifestFilePath = bagDirectory.toString() + "/manifest-sha256.txt";
+                System.out.println("MiscServiceFixity - manifestFilePath "+manifestFilePath);
+                FileWriter manifestStream = null;
+                for (DcsFile dcsFile : dcsFiles) {
+                    String fileName = dcsFile.getName();
+                    System.out.println("MiscServiceFixity - FileName "+fileName);
+                    for (DcsFixity fixity:dcsFile.getFixity()){
+                        System.out.println("MiscServiceFixity - getAlgorithm "+fixity.getAlgorithm());
+                        if(fixity.getAlgorithm().equals("SHA-1")){
+                            String strCheckSum = fixity.getValue().toString();
+                            System.out.println("MiscServiceFixity - fixity Algo " + fixity.getAlgorithm());
+                            try {
+                                manifestStream = new FileWriter(manifestFilePath,true);
+                                manifestInfo = new BufferedWriter(manifestStream);
+                                manifestInfo.write(strCheckSum+"\tdata/"+fileName+"\n");
+                                manifestInfo.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                            }
 
-        Collection<DcsFile> dcsFiles = sip.getFiles();
-        manifestFilePath = bagDirectory.toString() + "/manifest-sha256.txt";
-        System.out.println("MiscServiceFixity - manifestFilePath "+manifestFilePath);
-        FileWriter manifestStream = null;
-        for (DcsFile dcsFile : dcsFiles) {
-            String fileName = dcsFile.getName();
-            System.out.println("MiscServiceFixity - FileName "+fileName);
-            for (DcsFixity fixity:dcsFile.getFixity()){
-                System.out.println("MiscServiceFixity - getAlgorithm "+fixity.getAlgorithm());
-                if(fixity.getAlgorithm().equals("SHA-1")){
-                    String strCheckSum = fixity.getValue().toString();
-                    System.out.println("MiscServiceFixity - fixity Algo " + fixity.getAlgorithm());
-                    try {
-                        manifestStream = new FileWriter(manifestFilePath,true);
-                        manifestInfo = new BufferedWriter(manifestStream);
-                        manifestInfo.write(strCheckSum+"\tdata/"+fileName+"\n");
-                        manifestInfo.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                        }
                     }
 
                 }
-            }
 
+                String bagPath = bagDirectory.toString();
+                //FileWriter manifestStream = null;
+                System.out.println("TagManifestBagPath : "+bagPath);
+                String manifestFilePath = bagPath + "/tagmanifest-sha256.txt";
+                try {
+                    manifestStream = new FileWriter(manifestFilePath);
+                    manifest = new BufferedWriter(manifestStream);
+                    File dir = new File(bagPath);
+                    File[] dirListing = dir.listFiles();
+                    if(dirListing != null){
+                        for(File child : dirListing){
+                            if (!child.isDirectory()) {
+                                String file = child.toString();
+                                int pos;
+                                pos = file.lastIndexOf('/');
+                                String fileName = file.substring(pos+1);
+                                System.out.println("TagFileName is :"+fileName);
+
+                                if (!fileName.equals("tagmanifest-sha256.txt")) {
+                                    try{
+                                        String fixity = generateCheckSum(file,"SHA1");
+                                        manifest.write(fixity + "  " + fileName+"\n");
+                                    }catch(Exception e){
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    manifest.close();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
         }
+    }
+
+    private String generateCheckSum(String filePath, String algorithm) throws Exception{
+        System.out.println("ManifestGenerateCheckSum:" + filePath);
+        MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
+        FileInputStream fileInputStream = new FileInputStream( filePath);
+        byte[] dataBytes = new byte[1024];
+        int bytesRead = 0;
+        while((bytesRead = fileInputStream.read(dataBytes)) != -1){
+            messageDigest.update(dataBytes,0,bytesRead);
+        }
+        byte[] digestBytes = messageDigest.digest();
+        StringBuffer sb = new StringBuffer("");
+        for (int i = 0; i < digestBytes.length; i++) {
+            sb.append(Integer.toString((digestBytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        System.out.println(algorithm+" Checksum for the file: " + sb.toString());
+        fileInputStream.close();
+        return sb.toString();
 
     }
 }
