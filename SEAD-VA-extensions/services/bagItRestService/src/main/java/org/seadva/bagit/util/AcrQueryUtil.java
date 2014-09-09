427cf096091b42e8deec5d1e0de4ca2591d8ab85
Isuru Suriarachchi <isurues@gmail.com>
2014-09-09 17:09:30 -0400

    Fixing a json array parsing issue

--- SEAD-VA-extensions/services/bagItRestService/src/main/java/org/seadva/bagit/util/AcrQueryUtil.java
+++ SEAD-VA-extensions/services/bagItRestService/src/main/java/org/seadva/bagit/util/AcrQueryUtil.java
@@ -26,19 +26,20 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AcrQueryUtil{
+public class AcrQueryUtil {
 
     String query;
-    public String getJsonResponse(MediciInstance t_instance,String predicate, String tagId) throws IOException, JSONException {
-        query ="SELECT ?object WHERE { "+
-                "<"+tagId+"> <"+predicate+"> ?object ."+
+
+    public String getJsonResponse(MediciInstance t_instance, String predicate, String tagId) throws IOException, JSONException {
+        query = "SELECT ?object WHERE { " +
+                "<" + tagId + "> <" + predicate + "> ?object ." +
                 " }";
-        return getProxy(t_instance).getSparqlJSONResponse("query="+query);
+        return getProxy(t_instance).getSparqlJSONResponse("query=" + query);
     }
 
-    protected MediciProxy getProxy(MediciInstance t_instance){
+    protected MediciProxy getProxy(MediciInstance t_instance) {
         MediciProxy _mp = new MediciProxy();
-        _mp. setCredentials(t_instance.getUser(), t_instance.getPassword(),
+        _mp.setCredentials(t_instance.getUser(), t_instance.getPassword(),
                 t_instance.getUrl(),
                 t_instance.getRemoteAPI());
         return _mp;
@@ -52,58 +53,54 @@ public class AcrQueryUtil{
         JSONArray resultArray = new JSONArray();
         try {
             jsonObject = new JSONObject(json);
-
-
-            JSONObject resultObject = jsonObject.getJSONObject("sparql")
-                    .getJSONObject("results");
-
-            if(resultObject.has("result")){
-                try{
+            JSONObject sparqlObject = jsonObject.getJSONObject("sparql");
+            Object child = sparqlObject.get("results");
+            // first child can be empty. have to check
+            if ((child instanceof String) && ("".equals(child.toString())) ) {
+                return result;
+            }
+            JSONObject resultObject = sparqlObject.getJSONObject("results");
+            if (resultObject.has("result")) {
+                try {
                     resultArray.put(0, resultObject.getJSONObject("result"));
-                }
-                catch(Exception e){
+                } catch (Exception e) {
                     resultArray = resultObject.getJSONArray("result");
                 }
-
             }
-        }catch (JSONException e) {
+        } catch (JSONException e) {
             e.printStackTrace();
         }
-        try{
-            for(int i =0; i< resultArray.length();i++){
+        try {
+            for (int i = 0; i < resultArray.length(); i++) {
                 JSONArray binding = resultArray.getJSONObject(i).getJSONArray("binding");
                 String title = "";
                 String id = "";
-                for(int j =0; j< binding.length();j++){
-                    if(((String)binding.getJSONObject(j).get("name")).equalsIgnoreCase("id"))
-                        id = (String)binding.getJSONObject(j).get("literal");
-                    if(id.equalsIgnoreCase(attribute))
-                        if(((String)binding.getJSONObject(j).get("name")).equalsIgnoreCase("name"))
-                        {
-                            title = (String)binding.getJSONObject(j).get("literal");
+                for (int j = 0; j < binding.length(); j++) {
+                    if (((String) binding.getJSONObject(j).get("name")).equalsIgnoreCase("id"))
+                        id = (String) binding.getJSONObject(j).get("literal");
+                    if (id.equalsIgnoreCase(attribute))
+                        if (((String) binding.getJSONObject(j).get("name")).equalsIgnoreCase("name")) {
+                            title = (String) binding.getJSONObject(j).get("literal");
                             result.add(title);
                         }
                 }
             }
-        }
-        catch (JSONException e) {
+        } catch (JSONException e) {
             JSONObject binding = null;
-            for(int i =0; i< resultArray.length();i++){
+            for (int i = 0; i < resultArray.length(); i++) {
                 try {
-
                     binding = resultArray.getJSONObject(i).getJSONObject("binding");
                     String title = "";
-                    String key = (String)binding.get("name");
-                    if(key.equalsIgnoreCase(attribute))
-                    {
-                        if(binding.get("literal") instanceof String)
-                            title = (String)binding.get("literal");
-                        else{
-                            if(binding.get("literal") instanceof  Boolean){
-                                result.add(String.valueOf((Boolean)binding.get("literal")));
+                    String key = (String) binding.get("name");
+                    if (key.equalsIgnoreCase(attribute)) {
+                        if (binding.get("literal") instanceof String)
+                            title = (String) binding.get("literal");
+                        else {
+                            if (binding.get("literal") instanceof Boolean) {
+                                result.add(String.valueOf((Boolean) binding.get("literal")));
                                 continue;
                             }
-                            JSONObject temp = (JSONObject)binding.get("literal");
+                            JSONObject temp = (JSONObject) binding.get("literal");
                             title = temp.get("content").toString();
                         }
                         result.add(title);
@@ -112,7 +109,7 @@ public class AcrQueryUtil{
                     assert binding != null;
                     String value = null;
                     try {
-                        value = (String)binding.get("uri");
+                        value = (String) binding.get("uri");
                     } catch (JSONException e2) {
                         e2.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
                     }
